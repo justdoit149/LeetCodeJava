947340a4c9ba8460ba8404205925d0932fc4547a
shanyudong <1427060889@qq.com>
2023-09-10 16:53:37 +0800

    1

---
+++ 0001_0100/T0011.java
@@ -0,0 +1,28 @@
+public class T0011 {
+    //思考：如果目前左指针在i，右指针在j，不妨左边小，由于短板效应，右边再大也不可能让高增加
+    //而如果此时左指针不动，右指针左移的这些情况，高没增加但是底变小了，一定不是最大值
+    //所以一开始i在左j在右，谁指向的高小谁就向内移动一步，这样一定不会漏掉最大值
+    //这个算法只需要遍历一次数组，时间复杂度O(n)
+    //这是典型的“双指针”类型，直接暴力枚举是n^2，相比暴力，每一次指针移动会排除掉O(n)种情况
+    //例如左i右j，变成左i右j-1时，就排除掉了i到j、i+1到j、i+2到j……、j-1到j。
+    //而暴力枚举是一定能找到的，因此只要保证排除掉的这些里没有待求的情况，则剩下的肯定有max值。
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length-1,max=0,s;
+        while(l<r){
+            s = (r-l)*Math.min(height[l],height[r]);
+            if(s>max){
+                max = s;
+            }
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 0001_0100/T0015.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+//思路：定一动二，“二”的和要为“一”的相反数
+//排好序的数组里找两个和为-nums[i]的，经典的双指针，小于则移动左边、大于则移动右边
+//复杂度是O(n^2)
+//注意去重。由于nums已经排好序，有nums[i]<=nums[l]<=nums[r]
+//因此完全可以先用二维数组处理，然后对二维数组排序（默认先看第一列再看第二列再第三列）然后去重
+//去完重之后再拷贝进ArrayList里。（不太能对这个比较复杂的ArrayList排序）
+
+public class T0015 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //List是个接口，不能实例化，Arraylist等类实现了这个接口，因此这里需要new的是ArrayList.
+        //编译时ans视为List，接口里有相应声明的属性和方法，能通过编译
+        //运行时ans视为ArrayList，相应的方法已经实现，能正确运行。
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        int l,r,sum;
+        for(int i = 0; i < nums.length-2; i++){
+            //去重1：首个一样的话，找到的肯定是和之前找过的重复的
+            //而如果首个都不一样那肯定不是重复的，因此这里保证了外层for进行n次不会出现重复
+            //处理如[-3,-3,0,1,2,3]的情况
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            l = i + 1;
+            r = nums.length-1;
+            while(l<r){
+                sum = nums[l] + nums[r] + nums[i];
+                if(sum < 0){
+                    l++;
+                }else if(sum > 0){
+                    r--;
+                }else{
+                    //Array到ArrayList，可以用Arrays.asList，其参数可以是完整写出这个数组元素
+                    //也可以是对象型数组的数组名，但不能是基本数据类型的数组。
+                    List<Integer> temp = Arrays.asList(nums[i],nums[l],nums[r]);
+                    //！！！注意：基本类型用==，引用类型只能用.equals()判等
+                    if(ans.isEmpty() || !ans.get(ans.size()-1).equals(temp)){
+                        //去重2：定i时第一个重复，只看后两个。
+                        //由于已经nums排好序，这样产生的重复一定是相邻的，只需要看列表顶即可
+                        ans.add(temp);
+                    }
+                    l++;
+                    r--;
+                }
+            }
+        }
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
