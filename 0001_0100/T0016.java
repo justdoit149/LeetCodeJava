4a3f30d77616391ea31307ddb32b1f228bd8e335
shanyudong <1427060889@qq.com>
2023-09-11 11:56:22 +0800

    9.11 --T0016

---
+++ 0001_0100/T0016.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class T0016 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int l,r,sum,ans=nums[0]+nums[1]+nums[2];
+        for(int i = 0; i < nums.length-2; i++){
+            l = i + 1;
+            r = nums.length - 1;
+            while(l<r){
+                sum = nums[i] + nums[l] + nums[r];
+                if(Math.abs(target-sum) < Math.abs(target-ans)){
+                    ans = sum;
+                }
+                if(sum > target){
+                    r--;
+                }else{
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
