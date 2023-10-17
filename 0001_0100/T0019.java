3026de000cdcc51e24466d44a6712e0ed086cb67
shanyudong <1427060889@qq.com>
2023-10-17 14:43:17 +0800

    1

---
+++ 0001_0100/T0019.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class T0019 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode l = head, r = head, ll = null;
+        for(; n > 0; n--){
+            r = r.next;
+        }
+        while(r != null){
+            ll = l;//如果能进这个循环说明l != head，不进这个循环说明l == head用不上ll了
+            l = l.next;
+            r = r.next;
+        }
+        if(l == head){
+            head = l.next;
+        }else{//l是尾的时候l.next == null，也适用于else
+            ll.next = l.next;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
