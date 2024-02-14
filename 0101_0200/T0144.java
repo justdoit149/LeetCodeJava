d4ec904ac1ff650a71e954ed52287b75f3576778
shanyudong <1427060889@qq.com>
2024-02-14 19:49:27 +0800

    144

--- 0101_0200/T0144.java
+++ 0101_0200/T0144.java
@@ -4,28 +4,64 @@ import java.util.Stack;
 
 
 public class T0144 {
-    public List<Integer> preorderTraversal(TreeNode root) {
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> ans = new ArrayList<>();
+    //     preOrder(ans,root);
+    //     return ans;
+    // }
+
+    // public void preOrder(List<Integer> ans, TreeNode root){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     ans.add(root.val);
+    //     preOrder(ans, root.left);
+    //     preOrder(ans, root.right);
+    // }
+
+    // public List<Integer> preorderTraversal(TreeNode root){
+    //     List<Integer> ans = new ArrayList<>();
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     if(root == null) return ans;
+    //     TreeNode node = root;
+    //     while (!stack.isEmpty() || node != null) {
+    //         while (node != null) {
+    //             ans.add(node.val);
+    //             stack.push(node);
+    //             node = node.left;
+    //         }
+    //         node = stack.pop();
+    //         node = node.right;
+    //     }
+    //     return ans;
+    // }
+
+    public List<Integer> preorderTraversal(TreeNode root){
         List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode temp = root;
-        // String s = "";
-        // s.length();
-        // int[] s0 = {0};
-        // s0.length + 1;
-        while(true){
-            while(temp != null){
-                ans.add(temp.val);
-                stack.add(temp);
-                temp = temp.left;
-            }
-            if(!stack.isEmpty()) {
-                temp = stack.pop().right;
+        TreeNode nowPointer = root;
+        TreeNode prePointer = null;
+        while(nowPointer != null){
+            if(nowPointer.left == null){
+                ans.add(nowPointer.val);
+                prePointer = nowPointer;
+                nowPointer = nowPointer.right;
             }else{
-                return ans;
+                prePointer = nowPointer.left;
+                while(prePointer.right != null && prePointer.right != nowPointer){
+                    prePointer = prePointer.right;
+                }
+                if(prePointer.right == null){
+                    ans.add(nowPointer.val);
+                    prePointer.right = nowPointer;
+                    prePointer = nowPointer;
+                    nowPointer = nowPointer.left;
+                }else{
+                    prePointer.right = null;
+                    nowPointer = nowPointer.right;
+                }
             }
         }
-        
+        return ans;
     }
 
     public class TreeNode {
